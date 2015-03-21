41f77c5efcbd69fa6b2af584ba020d9e43f9bf25
bbczai <cchzai@yahoo.ca>
2015-03-21 00:20:42 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        cache <- NULL
+        set <- function(y) {
+                cache <<- y
+                
+        }
+        
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        if (!is.null(cache) {
+                message("getting cached inverse of matrix")
+                return(cache)
+        }
         ## Return a matrix that is the inverse of 'x'
 }
